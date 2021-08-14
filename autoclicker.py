fbf208ad7a88a6e87003f1a1f1f862b868568c1c
connor lol <80634435+yesconnorpoggershaha@users.noreply.github.com>
2021-08-14 08:47:11 +0100

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -2,5 +2,7 @@
 
 import pyautogui, time
 time.sleep(5) #// will wait 5 seconds to click so it doesnt start clicking right away, you can change this if you want it to start straight away or wait a bit longer :)
-for i in range (9999999): #// the number is how many times this program to click. you can make it infinite by making it like 9999 or something.
+for i in range (200): #// the number is how many times this program to click. you can make it infinite by making it like 9999 or something.
 	pyautogui.leftClick() #// You can enter co-ordinates in the brackets if you want to get a precise location for where it clicks
+
+#// Ik this is not fast as fuck but it's meant to be used on minecraft servers so it's not detectable. This also works on roblox.
